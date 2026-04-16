902394f6f9e58dadfb1e39a896cd8a7441983a4f
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-04-15 22:03:44 -0400

    Pagos | Inicialización de el microservicio para los pagos (archivos iniciales generados)

---
+++ ruki-backend/payment/src/test/java/com/ruki/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruki.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
