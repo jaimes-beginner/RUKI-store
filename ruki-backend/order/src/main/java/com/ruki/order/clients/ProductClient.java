4138d5f6e76c3b2a76db5ed4dfb1d58a5288e1fc
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-04-15 20:46:04 -0400

    Orden/pedido | Para poder comunicarnos con el servicio de pedidos y poder restar stock

--- ruki-backend/order/src/main/java/com/ruki/order/clients/ProductClient.java
+++ ruki-backend/order/src/main/java/com/ruki/order/clients/ProductClient.java
@@ -3,16 +3,17 @@ package com.ruki.order.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.ruki.order.requests.ProductClientResponse;
 
-/* 
-    Se conecta al Microservicio de Productos 
-    en el puerto 8081 para obtener la información 
-    de un producto específico
-*/
 @FeignClient(name = "product-service", url = "${app.clients.product-service.url}")
 public interface ProductClient {
 
     @GetMapping("/api-ruki/products/{id}")
     ProductClientResponse getProductById(@PathVariable("id") Long id);
+
+    @PutMapping("/api-ruki/products/{id}/discount-stock")
+    void discountStock(@PathVariable("id") Long id, @RequestParam("quantity") Integer quantity);
+    
 }
