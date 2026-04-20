bbaeb5e09187ca176eaad63292232cb1de9fbb5e
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-04-20 17:05:40 -0400

    Gateway | Creación del microservicio Gateway para enrutarlos con los 4 microservicios restantes

---
+++ ruki-backend/ruki-gateway/src/main/java/com/ruki/ruki_gateway/RukiGatewayApplication.java
@@ -0,0 +1,32 @@
+package com.ruki.ruki_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class RukiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RukiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*")); // En producción pondremos la URL de tu React
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+
+}
---
+++ ruki-backend/ruki-gateway/src/test/java/com/ruki/ruki_gateway/RukiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruki.ruki_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RukiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
