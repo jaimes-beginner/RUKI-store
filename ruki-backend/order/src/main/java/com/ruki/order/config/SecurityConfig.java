3debf23ee5d8958a940b402ce9a8a4e8cc87f3b8
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-04-21 02:14:52 -0400

    Usuario | Activar el CORS de seguridad

--- ruki-backend/order/src/main/java/com/ruki/order/config/SecurityConfig.java
+++ ruki-backend/order/src/main/java/com/ruki/order/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(Customizer.withDefaults())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
 
