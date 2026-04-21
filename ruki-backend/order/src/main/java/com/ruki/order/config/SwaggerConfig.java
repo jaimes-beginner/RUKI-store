29351ef3a1261c47f2731495d4cde5bb398d29b5
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-04-21 18:05:08 -0400

    General | Configurando la 'seguridad' para Swagger

---
+++ ruki-backend/order/src/main/java/com/ruki/order/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.ruki.order.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "RUKI API", version = "v1"),
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .servers(List.of(new Server().url("/")))
+            .info(new io.swagger.v3.oas.models.info.Info()
+                .title("RUKI Microservices API")
+                .description("Documentación centralizada de microservicios con autenticación JWT")
+                .version("1.0.0"));
+    }
+
+}
