fa89555123bc57a37698f8b092a16c2c262472ac
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-17 01:55:50 -0400

    Usuario | Mejora en tiempo de respuesta al ser asincrónico

--- ruki-backend/user/src/main/java/com/ruki/user/UserApplication.java
+++ ruki-backend/user/src/main/java/com/ruki/user/UserApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Gateway base URL")})
 @SpringBootApplication
+@EnableAsync
 public class UserApplication {
 
 	public static void main(String[] args) {
