562d781f273ff024ae49806664561b1a05bbbde4
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-16 22:33:40 -0400

    Usuario | Validación en tallas, ahora son obligatorias

--- ruki-backend/order/src/main/java/com/ruki/order/requests/OrderItemRequest.java
+++ ruki-backend/order/src/main/java/com/ruki/order/requests/OrderItemRequest.java
@@ -1,5 +1,6 @@
 package com.ruki.order.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -26,6 +27,7 @@ public class OrderItemRequest {
     @Positive(message = "La cantidad debe ser mayor a cero")
     private Integer quantity;
 
+    @NotBlank(message = "La talla del producto es obligatoria")
     private String size;
     
 }
