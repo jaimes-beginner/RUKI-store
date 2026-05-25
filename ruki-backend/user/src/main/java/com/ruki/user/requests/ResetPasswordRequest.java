e72804f578ae8fac83d734b1c644fa4419e00ce3
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-25 17:18:35 -0400

    REMASTER | Añadida validación @Size(min = 6) al campo newPassword para reforzar la seguridad y la integridad de los datos.

--- ruki-backend/user/src/main/java/com/ruki/user/requests/ResetPasswordRequest.java
+++ ruki-backend/user/src/main/java/com/ruki/user/requests/ResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package com.ruki.user.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size; 
 import lombok.Data;
 
 @Data
@@ -10,6 +11,7 @@ public class ResetPasswordRequest {
     private String token;
 
     @NotBlank(message = "La nueva contraseña es obligatoria")
+    @Size(min = 8, message = "La nueva contraseña debe tener al menos 8 caracteres")
     private String newPassword;
-
+    
 }
