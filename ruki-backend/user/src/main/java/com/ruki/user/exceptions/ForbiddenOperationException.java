367bebc160ac15ef4dbf2c2be66a0bbac00c6539
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-25 17:13:20 -0400

    REMASTER | Añadido @ResponseStatus(HttpStatus.FORBIDDEN) y así proporcionar un mapeo automático a 403 Forbidden si la excepción no es capturada por un ExceptionHandler específico.

--- ruki-backend/user/src/main/java/com/ruki/user/exceptions/ForbiddenOperationException.java
+++ ruki-backend/user/src/main/java/com/ruki/user/exceptions/ForbiddenOperationException.java
@@ -1,5 +1,9 @@
 package com.ruki.user.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class ForbiddenOperationException extends RuntimeException {
 
     public ForbiddenOperationException(String message) {
