210c9a68ba2a8789be6eef51d56e39093d71d699
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-25 17:19:38 -0400

    REMASTER | Reemplazo de @Data, @NoArgsConstructor, @AllArgsConstructor por @Value y @Builder para hacer el DTO de respuesta inmutable y mejorar la legibilidad.

--- ruki-backend/user/src/main/java/com/ruki/user/requests/UserResponse.java
+++ ruki-backend/user/src/main/java/com/ruki/user/requests/UserResponse.java
@@ -1,27 +1,19 @@
 package com.ruki.user.requests;
 
 import com.ruki.user.entities.Role;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Value; 
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Value 
+@Builder 
 public class UserResponse {
 
-    /*
-        Este es el formato que va a tener la respuesta del
-        usuario, los datos que devolverá para una respuesta 
-        mas limpia, sin mostrar la constraseña aunque esté hasheada
-    */
-
-    private Long id;                    
-    private String email;             
-    private String firstName;       
-    private String lastName;          
-    private Role role;                 
-    private LocalDateTime createdAt;    
-
+    Long id;
+    String email;
+    String firstName;
+    String lastName;
+    Role role;
+    LocalDateTime createdAt;
+    
 }
