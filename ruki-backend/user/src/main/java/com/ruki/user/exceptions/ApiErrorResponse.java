67c0a164e3eb49a4168ecc07925bf0f19a881eef
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-25 17:12:57 -0400

    REMASTER | Añadido @JsonFormat al campo timestamp para asegurar un formato estándar ISO 8601 para las marcas de tiempo en las respuestas JSON

--- ruki-backend/user/src/main/java/com/ruki/user/exceptions/ApiErrorResponse.java
+++ ruki-backend/user/src/main/java/com/ruki/user/exceptions/ApiErrorResponse.java
@@ -1,11 +1,17 @@
 package com.ruki.user.exceptions;
 
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record ApiErrorResponse(
-        int status,
+        int status,                                             
         String error,
         String message,
+
+        /*
+             Ahora con formato ISO 8601
+        */
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") 
         LocalDateTime timestamp
 ) {
 }
