195c9e8a0054471c979ed72c62d7d96917cad99c
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-25 17:15:49 -0400

    REMASTER | Ajuste de nombres de métodos de consulta a convenciones de JPA (findByUserIdAndActiveTrue, findByUserId). Añadido Optional<Address> findByIdAndUserId(Long addressId, Long userId). Todo esto para mejorar la legibilidad, la consistencia y proporcionar un método útil para la validación de IDOR

--- ruki-backend/user/src/main/java/com/ruki/user/repositories/AddressRepository.java
+++ ruki-backend/user/src/main/java/com/ruki/user/repositories/AddressRepository.java
@@ -1,20 +1,27 @@
 package com.ruki.user.repositories;
 
 import com.ruki.user.entities.Address;
-import com.ruki.user.requests.AddressResponse;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional; 
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    
-    /* 
-        Obtener todas las direcciones 
-        que tiene un usuario por su ID
+
+    /*
+        Obtener todas las direcciones activas de un usuario por su ID
     */
-    List<Address> findAllByUserId(Long userId);
-    List<Address> findByUser_IdAndActiveTrue(Long userId);
+    List<Address> findByUserIdAndActiveTrue(Long userId);
+
+    /*
+        Obtener todas las direcciones (activas e inactivas) de un usuario por su ID
+    */
+    List<Address> findByUserId(Long userId);
+
+    /*
+        Obtener una dirección específica por su ID y el ID del usuario al que pertenece
+    */
+    Optional<Address> findByIdAndUserId(Long addressId, Long userId);
 
 }
