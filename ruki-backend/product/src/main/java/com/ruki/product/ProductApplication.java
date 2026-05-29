6396c5b76796e3f8f536234bf1b7a1254ea4539e
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-28 23:58:42 -0400

    REMASTER | Se añadió la anotación @EnableFeignClients para habilitar la comunicación con otros microservicios a través de Feign, preparando el terreno para futuras integraciones.

--- ruki-backend/product/src/main/java/com/ruki/product/ProductApplication.java
+++ ruki-backend/product/src/main/java/com/ruki/product/ProductApplication.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients; 
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Gateway base URL")})
 @SpringBootApplication
+@EnableFeignClients
 public class ProductApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
     }
+
 }
