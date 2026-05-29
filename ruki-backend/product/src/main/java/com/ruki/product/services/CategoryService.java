80df87be1f1c07266d8de8aeb95864c63be30194
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-28 23:56:40 -0400

    REMASTER | Se extendió la interfaz del servicio de categorías para incluir un nuevo método updateCategory(), completando así las operaciones CRUD básicas para la gestión de categorías.

--- ruki-backend/product/src/main/java/com/ruki/product/services/CategoryService.java
+++ ruki-backend/product/src/main/java/com/ruki/product/services/CategoryService.java
@@ -6,17 +6,10 @@ import java.util.List;
 
 public interface CategoryService {
 
-    /*
-        Interfaz del servicio de categoría, definendo 
-        que tendrá la lógica de negocio para las caterogrías.
-        En este caso tiene para; Crear una categoría, obtener 
-        todas las categorías activas, obtener una categoría por 
-        su ID y desactivar una categoría.
-    */
-
     CategoryResponse createCategory(CategoryCreate request);
     List<CategoryResponse> getAllActiveCategories();
     CategoryResponse getCategoryById(Long id);
     void deactivateCategory(Long id);
-
+    CategoryResponse updateCategory(Long id, CategoryCreate request); 
+    
 }
