5a7ef348b0a14622b4cba1ef41d0aafda7364796
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-28 23:57:34 -0400

    REMASTER | Se reordenaron los métodos en la interfaz del servicio de productos para mejorar la legibilidad y la consistencia.

--- ruki-backend/product/src/main/java/com/ruki/product/services/ProductService.java
+++ ruki-backend/product/src/main/java/com/ruki/product/services/ProductService.java
@@ -3,22 +3,10 @@ package com.ruki.product.services;
 import com.ruki.product.requests.ProductCreate;
 import com.ruki.product.requests.ProductResponse;
 import com.ruki.product.requests.ProductUpdate;
-
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductService {
-
-    /*
-        Interfaz para definir que debe tener el 
-        servicio de productos, crear producto, obtener todos 
-        los productos activos, obtener productos por 
-        categoría, obtener producto por ID, desactivar 
-        producto, descontar stock, actualizar producto, agregar 
-        stock en caso de compras fallidas, obtener productos
-        recientes, obtener productos en oferta para buscar según 
-        los parámetros de filtrado dinámico
-    */
 
     ProductResponse createProduct(ProductCreate request);
     List<ProductResponse> getAllActiveProducts();
@@ -27,9 +15,9 @@ public interface ProductService {
     void deactivateProduct(Long id);
     ProductResponse updateProduct(Long id, ProductUpdate request);
     void addStock(Long id, Integer quantity, String size);
+    void discountStock(Long id, Integer quantity, String size); 
     List<ProductResponse> getNewArrivals();
     List<ProductResponse> getSaleProducts();
     List<ProductResponse> filterProducts(Long categoryId, String size, BigDecimal minPrice, BigDecimal maxPrice, String sort);
-    void discountStock(Long id, Integer quantity, String size);
 
 }
