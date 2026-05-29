8352910af6140074443dc6d8d0d5a177ac9a80db
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-05-28 23:53:15 -0400

    REMASTER | Se añadieron validaciones @Size para el campo 'name' para asegurar la integridad de los datos y alinearlos con los límites de la base de datos. Se incorporó la anotación @Builder para facilitar la creación de objetos de solicitud de forma más legible.

--- ruki-backend/product/src/main/java/com/ruki/product/requests/CategoryCreate.java
+++ ruki-backend/product/src/main/java/com/ruki/product/requests/CategoryCreate.java
@@ -1,18 +1,20 @@
 package com.ruki.product.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size; 
+import lombok.AllArgsConstructor; 
+import lombok.Builder; 
 import lombok.Data;
+import lombok.NoArgsConstructor; 
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoryCreate {
 
-    /* 
-        Request/requerimientos para 
-        crear una nueva categoría, en 
-        este caso solo el nombre del mismo.
-    */
-
     @NotBlank(message = "El nombre de la categoría es obligatorio")
+    @Size(min = 2, max = 100, message = "El nombre de la categoría debe tener entre 2 y 100 caracteres") 
     private String name;
-
+    
 }
