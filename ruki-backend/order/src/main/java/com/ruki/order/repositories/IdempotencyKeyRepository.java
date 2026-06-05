48f87ae2c4fd884ef25917beceeac5508d1855a6
Jaime Hermosilla <jaimelearn1@gmail.com>
2026-06-04 20:48:11 -0400

    IDEMPOTENCIA | Implementación de la el repositorio para la clave de idempotencia que vendrá por parte del front-end

---
+++ ruki-backend/order/src/main/java/com/ruki/order/repositories/IdempotencyKeyRepository.java
@@ -0,0 +1,10 @@
+package com.ruki.order.repositories;
+
+import com.ruki.order.entities.IdempotencyKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IdempotencyKeyRepository extends JpaRepository<IdempotencyKey, String> {
+
+}
